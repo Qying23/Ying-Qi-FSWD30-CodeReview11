a2a757a9afe01ced0365bb6d99e979168d87538a
qying23 <qying23@gmail.com>
2018-02-20 09:23:03 +0100

    d

--- php/cars_location4.php
+++ php/cars_location4.php
@@ -7,7 +7,7 @@ $dbname = "cr11_Ying_Qi_php_car_rental";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Hauptbahnhof</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
